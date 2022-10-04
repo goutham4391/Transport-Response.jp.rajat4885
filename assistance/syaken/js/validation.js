7cfe0cd3becfc394854ff775119f75c3feb73f84
rajat4885 <100342306+rajat4885@users.noreply.github.com>
2022-10-04 17:53:26 +0530

    initil

---
+++ assistance/syaken/js/validation.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+  // 査定用 validation
+  var validateFunc = function() {
+   $(".validate").validation({
+      msgs: {
+      zip: "7桁の郵便番号を入力してください",
+      submit: "郵便番号は入力必須です。<br />7桁の郵便番号を正しく入力してく ださい。"
+     },
+      extension: {
+      top: 20,
+      left: -180
+      }
+    });
+  }
+
+  $(".validate").click(function() {
+    // 古いバリデーションを削除
+    $("div.validationTip,div#dlgmat,div#dlg").each(function(){ $(this).remove(); });
+    // 査定用 validation を有効化
+    $("#zipcode").addClass("required");
+    $("#zipcode").addClass("zip");
+
+    $(".sateibtn").removeAttr("disabled");
+
+    // validation 実行
+     validateFunc();
+});
+});
